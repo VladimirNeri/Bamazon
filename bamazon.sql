993e8ebf4bde6c8df278113954b1f95812820b39
obscura101 <vlad.neri@gmail.com>
2019-06-10 22:15:31 -0700

    updated sql

--- bamazon.sql
+++ bamazon.sql
@@ -1,34 +1,29 @@
+DROP DATABASE IF EXISTS bamazonDB
+
 CREATE DATABASE bamazonDB;
 
 USE bamazonDB;
 
-CREATE TABLE products(
-item_id INT NOT NULL AUTO_INCREMENT,
-product_name VARCHAR(100) NULL,
-department_name VARCHAR(100) NULL,
-price DECIMAL(10,2) NULL,
-stock_quantity INT(11) NULL,
+CREATE TABLE products (
+item_id INT(10) AUTO_INCREMENT NOT NULL,
+product_name VARCHAR(100) NOT NULL,
+department_name VARCHAR(100) NOT NULL,
+price DECIMAL(10,2) NOT NULL,
+stock_quantity INT(11) NOT NULL,
 PRIMARY KEY (item_id)
 );
 
-CREATE TABLE departments(
-department_id INT NOT NULL AUTO_INCREMENT,
-department_name VARCHAR(100) NULL,
-over_head_costs INT(11) NULL,
-PRIMARY KEY (department_id)
-);
-
-ALTER TABLE products
-MODIFY product_sales DECIMAL(10,2) DEFAULT "0.00";
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Computer", "Electronics", 1200.00, 10),
+("Notebook", "Electronics", 299.99, 8),
+("iPhone", "Electronics", 800.00, 25),
+("Sofa", "Furniture", 450.00, 15),
+("Table", "Furniture", 500.00, 20),
+("Shelf", "Furniture", 100.00, 15),
+("Eggs", "Groceries", 5.00, 200),
+("Milk", "Groceries", 3.00, 30),
+("Coffee", "Groceries", 15.00, 20),
+("Beer", "Groceries", 10.00, 10);
 
 SELECT * FROM products;
-SELECT * FROM departments;
 
-INSERT INTO departments (department_name, over_head_costs)
-VALUES ("Sporting Goods", 3000);
-
-DELETE FROM products WHERE product_name = "Loose Herbal Tea";
-
-UPDATE products
-SET price = 2.99
-WHERE product_name = "Herbal Tea";
